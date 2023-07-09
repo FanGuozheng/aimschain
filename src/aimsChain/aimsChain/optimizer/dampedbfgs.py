01e28ab28f6b5772add7a7a3b7fff42ee00c5e09
FanGuozheng <gfan@uni-bremen.de>
2023-07-10 00:59:10 +0200

    init commit

---
+++ src/aimsChain/aimsChain/optimizer/dampedbfgs.py
@@ -0,0 +1,142 @@
+import numpy as np
+from numpy.linalg import eigh, solve
+
+import pickle as cp
+
+class dampedBFGS(object):
+    def __init__(self, restart="hess", maxstep=0.04, 
+                 alpha = 70):
+        """BFGS optimizer.while force > 0.01:
+
+        Parameters:
+
+        restart: string
+            Pickle file used to store hessian matrix. 
+            load() will attempt to load existing Hessian from it.
+            dump() will dump the current Hessian to it. 
+        Maxstep: float
+            Used to set the maximum distance an atom can move per
+            iteration (default value is 0.04 A).
+        """
+        
+        if maxstep is not None:
+            if maxstep > 1.0:
+                raise ValueError('You are using a much too large value for ' +
+                                 'the maximum step size: %.1f A' % maxstep)
+            self.maxstep = maxstep
+        self.restart = restart
+        self.alpha = alpha
+
+    def initialize(self):
+        self.H = None
+        self.r0 = None
+        self.f0 = None
+
+        
+
+    def load(self):
+        """
+        load saved hessian, positions, and forces
+        """
+        import os.path as path
+        if path.isfile(self.restart):
+            hess = open(self.restart, 'r')
+            try:
+                self.H, self.r0, self.f0 = cp.load(hess)
+            except:
+                self.initialize()
+            hess.close()
+
+    def dump(self):
+        """
+        dump necessary values for future reference
+        """
+        hess = open(self.restart, 'wb')
+        cp.dump((self.H, self.r0, self.f0),
+                hess)
+        hess.close()
+
+    def step(self, r, f):
+        """
+        Tkae a single step
+        """
+        r = np.array(r)
+        f = np.array(f).flatten()
+
+        self.update(r, f)
+
+        dr = self.H.dot(f)
+
+        dr = dr.reshape(-1,3)
+        dr = self.determine_step(dr)
+        dr = np.reshape(dr, np.shape(r))
+
+        self.r0 = r.flatten()
+        self.f0 = f.flatten()
+
+
+        return r+dr
+
+    def determine_step(self, dr):
+        """Determine step to take according to maxstep
+        
+        Normalize all steps as the largest step. This way
+        we still move along the eigendirection.
+        """
+        
+        steplengths = (dr**2).sum(1)**0.5
+        maxsteplength = np.max(steplengths)
+        if maxsteplength >= self.maxstep:
+            dr *= self.maxstep / maxsteplength
+        
+        return dr
+
+    def update(self, r, f):
+
+        r = r.flatten()
+        f = f.flatten()
+        if self.H is None:
+            self.H = np.eye(len(r)) * (1.0/self.alpha)
+            self.r0 = r
+            self.f0 = f
+            return
+
+        r0 = self.r0
+        f0 = self.f0
+
+        sk = r.reshape(-1) - r0.reshape(-1)
+
+        if np.abs(sk).max() < 1e-7:
+            # Same configuration again (maybe a restart):
+            return
+
+        #force is negative!
+        
+        yk = f0.reshape(-1) - f.reshape(-1)
+        rhok=np.dot(yk,sk)
+        
+        #print sk
+        #print yk
+        theta = 1
+        thres = 0.2*np.dot(sk,self.H).dot(sk)
+        
+        if rhok < thres:
+            theta = (4*thres)/(5*thres-rhok)
+#            print "damped!"
+#            self.H = np.eye(len(r))*(0.1/self.alpha)
+            return
+        #yk =  theta * yk + (1 -theta)*np.dot(sk,self.H)
+        #BFGS update formula
+        I= np.eye(len(r))
+        try:
+            rhok = 1.0/(np.dot(yk,sk))
+        except ZeroDivisionError:
+            rhok = 1000.0
+        if np.isinf(rhok) or float(rhok) >=1000:
+            rhok = 1000.0
+        A1 = I - sk[:,np.newaxis] * yk[np.newaxis,:] * rhok
+        A2 = I - yk[:,np.newaxis] * sk[np.newaxis,:] * rhok
+        
+        self.H = (np.dot(A1,np.dot(self.H,A2)) 
+                  + rhok * np.outer(sk,sk))
+        
---
+++ src/aimsChain/tools/runchain.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+import distutils.dir_util as dir_util
+import shutil
+import numpy as np
+import pickle as cp
+
+from aimsChain.node import Path
+from aimsChain.node import Node
+from aimsChain.aimsio import read_aims
+from aimsChain.config import Control
+from aimsChain.interpolate import get_t
+from aimsChain.aimsio import write_mapped_aims, write_xyz, write_aims
+
+def run_aims(paths):
+    global control
+    while len(paths) > 0:
+        path = paths[0]
+        if control.restart:
+            save_restart(paths)
+        #remove the ending slash if it exist
+        if path[-1] == "/":
+            path = path[:-1]
+        #generate the name for output
+        filename = path[len(path)-path[::-1].index('/'):]+'.out'
+        
+        command = 'cd ' + path + ';' + control.run_aims + ' > ' + filename
+        subprocess.call(command, shell=True)
+        paths.remove(path)
+        if control.restart:
+            save_restart(paths)
+        
+    paths = []
+
+
+def initial_interpolation():
+    global control
+    global path
+    #set the initial and final image
+    ininode = Node(param = 0.0, 
+                   geometry = read_aims(control.ini))
+    finnode = Node(param = 1.0,
+                   geometry = read_aims(control.fin))
+
+    #does periodic transformation between initial and final node
+    #minimize distance between initial and final image atom-wise
+    #maybe this should be moved to another file...runchain is getting too messy
+    #leave it like this for now
+    if control.periodic_interp and finnode.geometry.lattice != None:
+        lattice = finnode.geometry.lattice
+        initial_pos = ininode.positions
+        curr_pos = finnode.positions
+        fin_pos = []
+        for i,atom_pos in enumerate(curr_pos):
+            dis = 1000000
+            pos = None
+            for a in [-1,0,1]:
+                for b in [-1,0,1]:
+                    for c in [-1,0,1]:
+                        pos_tmp = (atom_pos +
+                                   a*lattice[0] + b*lattice[1] + c*lattice[2])
+                        dis_tmp = np.sum(np.array(pos_tmp - initial_pos[i])**2)**0.5
+                        if dis_tmp <= dis:
+                            pos = pos_tmp
+                            dis = dis_tmp
+            fin_pos.append(pos)
+
+        finnode.positions = fin_pos                    
+
+
+    #fix the initial and final node    
+    ininode.fixed = True
+    finnode.fixed = True
+
+    #parse the externl geometry
+    try:
+        nodes = [ininode]
+        if control.ext_geo and os.path.isfile(str(control.ext_geo)):
+            geo = open(control.ext_geo)
+            lines = geo.readlines()
+            geo.close()
+            for line in lines:
+                inp = line.split()
+                if inp == []:
+                    continue
+                elif inp[0][0] == '#':
+                    continue
+                else:
+                    if os.path.isfile(inp[0]):
+                        nodes.append(Node(param = 0.5, 
+                                          geometry = read_aims(inp[0]), 
+                                          fixed = False))
+            nodes.append(finnode)
+            geos = []
+            for node in nodes:
+                geos.append(node.positions)
+            t = get_t(geos)
+            for i in range(len(t)):
+                nodes[i].param = t[i]
+            path.nodes = nodes
+            #if resample is turned on and n_image does not equal to 
+            #number of images from external source,
+            #then resample the path
+            if control.resample and control.nimage != (len(nodes)-2):
+                path.interpolate(control.nimage)
+
+    except:
+        print('!Error interprating the external geometries\n')
+        print('!Using standard interpolation method for initial geometries\n')
+
+    #if there were no external geometry, linear interpolate the image
+    if len(nodes) <= 2:
+        path.nodes = [ininode, finnode]
+        #interpolate the images
+        path.interpolate(control.nimage)
+
+def save_restart(path):
+    global restart_stage
+    global force
+    restart = open("iterations/restart_file", 'w')
+    cp.dump((path, restart_stage, force), restart)
+    restart.close()
+
+def read_restart():
+    global path_to_run
+    global force
+    global restart_stage
+    global path
+    file_exist = os.path.isfile("iterations/restart_file")
+    if file_exist:
+        restart = open("iterations/restart_file", 'r')
+        path_to_run, restart_stage, force = cp.load(restart)
+        path.read_path("iterations/path.dat")
+        path.load_nodes()
+        restart.close()
+    return file_exist
+
+def write_current():
+    global path
+    dir_name = "iteration%04d" % path.runs
+    dir_name = os.path.join("paths", dir_name)
+    try:
+        os.mkdir(dir_name)
+    except OSError:
+        pass
+    ener = []
+    climb = []
+    fixed = []
+    write_xyz(os.path.join(dir_name, "path.xyz"), path, control.xyz_lattice)
+    for i,node in enumerate(path.nodes):
+        ener.append(node.ener)
+        climb.append(node.climb)
+        fixed.append(node.fixed)
+        i += 1
+        file_name = os.path.join(dir_name, "image%03d.in" % i)
+        if control.map_unit_cell:
+            write_mapped_aims(file_name, node.geometry)
+        else:
+            write_aims(file_name, node.geometry)
+    ener = np.array(ener) - ener[0]
+    enerfile = open(os.path.join(dir_name, "ener.lst"), 'w')
+    for i,energy in enumerate(ener):
+        enerfile.write("image%03d\t%.10f" % (i+1, energy))
+        if climb[i]:
+            enerfile.write("\t CLIMB")
+        if fixed[i]:
+            enerfile.write("\t FIXED")
+        enerfile.write("\n")
+    enerfile.close()
+    pathfile = open(os.path.join(dir_name, "path.lst") ,'w')
+    for i,item in enumerate(path.get_paths()):
+        pathfile.write("image%03d\t%s" % (i+1,item))
+        if climb[i]:
+            pathfile.write("\t CLIMB")
+        if fixed[i]:
+            pathfile.write("\t FIXED")
+        pathfile.write("\n")
+    pathfile.close()
+        
+    
+
+force = 10.0
+control = Control()
+path = Path(control=control)
+restart_stage = 0
+is_restart = read_restart() and control.restart
+
+
+forcelog = open("forces.log", 'a')
+
+
+if not is_restart:
+    for directory in ["paths", "iterations", "optimized"]:
+        if os.path.isdir(directory):
+            shutil.rmtree(directory)
+    os.mkdir("paths")
+
+    
+    initial_interpolation()
+
+    #write directory for images
+    path_to_run = path.write_all_node()
+    path.write_path("iterations/path.dat")
+
+    forcelog.write("#Residual Forces in the system:\n")
+    forcelog.flush()
+
+if restart_stage == 0:
+    while force > control.thres:
+        run_aims(path_to_run)
+        path.load_nodes()
+        if control.method == "neb":
+            force = path.move_neb()
+        elif control.method == "string":
+            force = path.move_string()
+        write_current()
+        curr_runs = path.runs
+        if force > control.thres:
+            path.add_runs()
+            path_to_run = path.write_node()
+        forcelog.write('iteration%04d\t%16.16f \n' % (curr_runs,force))
+        forcelog.flush()
+        path.write_path("iterations/path.dat")
+    force = 10.0
+    forcelog.write("System has converged.\n")
+
+forcelog.close()
+
+if control.use_climb:
+    forcelog = open("climbing_forces.log", 'a')
+    if restart_stage != 1:
+        path.find_climb()
+        path.add_runs()
+        if control.climb_control != "control.in":
+            path_to_run = path.write_all_node(control.climb_control)
+        else:
+            path_to_run = path.write_node()
+
+        forcelog.write("#Residual Forces in the Climbing image:\n")
+        forcelog.flush()
+        restart_stage = 1
+
+
+    while force > control.climb_thres:
+        run_aims(path_to_run)
+        path.load_nodes()
+        force = path.move_climb()
+        write_current()
+        curr_runs = path.runs
+        if force > control.climb_thres:
+            path.add_runs()
+            path_to_run = path.write_node(control.climb_control)
+        forcelog.write('iteration%04d\t%16.16f \n' % (curr_runs, force))
+        forcelog.flush()
+        path.write_path("iterations/path.dat")
+    forcelog.write('Climbing image has converged.\n')
+    forcelog.close()
+
+try:
+    os.mkdir('optimized')
+except OSError:
+    pass
+
+for i,dir in enumerate(path.get_paths()):
+    i+=1
+    dir_util.copy_tree(dir, os.path.join('optimized', "image%03d" % i))
+write_xyz("optimized/path.xyz", path, control.xyz_lattice)
